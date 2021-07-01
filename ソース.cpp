2ac7e9c4fa5145abe6169f527dca26b1ec1bd7c2
Satori32 <Huga8989+githubf@gmail.com>
2021-07-01 16:59:27 +0900

    some change.

--- ソース.cpp
+++ ソース.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <cstdint>
 
-template<class T>
-T Detune(const T& N, const T& Max, const T& Step) {
-	T A = Max / Step;
+template<class T, class U>
+U Detune(const T& N, const U& Max, const U& Step) {
+	U A = Max / Step;
 	T B = N / A;
 
 	return B*Step;
 }
 
 int main() {
-	std::uint16_t N = 125;
+	std::uint16_t N = 17;
 	std::uint16_t Max = 256;
 	std::uint16_t Step = 16;
 
-	std::uint16_t A = Detune<std::uint16_t>(N, Max, Step);
+	std::uint16_t A = Detune(N, Max, Step);
 
 	std::cout << N << std::endl;
 	std::cout << Max << std::endl;
