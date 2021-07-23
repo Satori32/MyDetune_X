f117050a0fe1f108b9b9b16a3ae1ce83709b9da6
Satori32 <Huga8989+githubf@gmail.com>
2021-07-23 19:54:10 +0900

    add function.

--- ソース.cpp
+++ ソース.cpp
@@ -2,23 +2,49 @@
 #include <cstdint>
 
 template<class T>
-T Detune(const T& N, const T& Max, const T Step) {
+T Detune(const T& N, const T& Max, const T Step) {//separate region.
 	T A = Max / Step;
 	T B = N / A;
 
 	return B*Step;
 }
 
+template<class T>
+T Step(const T& N, const T& Max, const T Step) {//what number to i standing.
+	T A = Max / Step;
+	T B = N / A;
+
+	return B;
+}
+
 int main() {
-	std::uint16_t N = 17;
-	std::uint16_t Max = 256;
-	std::uint16_t Step = 16;
 
-	std::uint16_t A = Detune(N, Max, Step);
+	{
+		std::uint16_t N = 17;
+		std::uint16_t Max = 256;
+		std::uint16_t Step = 16;
 
-	std::cout << N << std::endl;
-	std::cout << Max << std::endl;
-	std::cout << Step<< std::endl;
-	std::cout << A << std::endl;
+		std::uint16_t A = Detune(N, Max, Step);
+
+		std::cout << N << std::endl;
+		std::cout << Max << std::endl;
+		std::cout << Step << std::endl;
+		std::cout << A << std::endl;
+	}
+	std::cout << "*-----------------------------*" << std::endl;
+	{
+		std::uint16_t N = 19;
+		std::uint16_t Max = 256;
+		std::uint16_t St = 16;
+
+		std::uint16_t A = Step(N, Max, St);
+
+		std::cout << N << std::endl;
+		std::cout << Max << std::endl;
+		std::cout << St << std::endl;
+		std::cout << A << std::endl;
+	}
+
+
 	return 0;
 }
